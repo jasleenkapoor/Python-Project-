c9f35ce3fd66a32220008108acb575bb7648c527
zhap066 <116880341+zhap066@users.noreply.github.com>
2022-11-15 12:21:22 +0000

    Update emlyns initial copy paste+save all numerical values to list.py

--- src/tdse/emlyns initial copy paste+save all numerical values to list.py
+++ src/tdse/emlyns initial copy paste+save all numerical values to list.py
@@ -112,7 +112,7 @@ for t in t_array:
 ######################
 
 
-def writetxt(array,filename,spacedwith,txtaxis):# a function that takes the inputed array and outputs it in the form of a txt document, with an optional axis
+def writetxt(array,filename,spacedwith,txtaxis):# a function that takes the inputted array and outputs it in the form of a txt document, with an optional axis
     with open(filename, 'w') as file:
         if txtaxis ==True:
             file.writelines(" Psi in Time-->")
