92f59351fe18099a9e5cdb3031b44abd0ab45d0d
zhap066 <116880341+zhap066@users.noreply.github.com>
2022-11-15 12:13:44 +0000

    Update save_psi.py
    Updating spelling mistakes

--- src/tdse/save_psi.py
+++ src/tdse/save_psi.py
@@ -1,5 +1,5 @@
-#parameters: array of np.square(np.abs(psi_list),a string that will seperate the data, a file name for txt, Ture for axis, Talse for no axis
-def writetxt(array,filename,spacedwith,txtaxis):# a function that takes the inputed array and outputs it in the form of a txt document, with an optional axis
+#parameters: array of np.square(np.abs(psi_list),a string that will separate the data, a file name for text file, True for axis, false for no axis
+def writetxt(array,filename,spacedwith,txtaxis):# a function that takes the inputted array and outputs it, in the form of a text document, with an optional axis
     with open(filename, 'w') as file:
         if txtaxis ==True:
             file.writelines(" Psi in Time-->")
