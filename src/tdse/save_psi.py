09484cba5c708672148441776c7d59f2d2f8fdc0
Jasleen Kapoor <jasleen@Jasleens-MacBook-Pro.local>
2022-11-16 15:52:34 +0000

    file has added doc.strings and comments

--- src/tdse/save_psi.py
+++ src/tdse/save_psi.py
@@ -1,8 +1,24 @@
-#parameters: array of np.square(np.abs(psi_list),a string that will separate the data, a file name for text file, True for axis, false for no axis
-def writetxt(array,filename,spacedwith,txtaxis):# a function that takes the inputted array and outputs it, in the form of a text document, with an optional axis
-    with open(filename, 'w') as file:
-        if txtaxis ==True:
-            file.writelines(" Psi in Time-->")
+
+def writetxt(array,filename,spacedwith,txtaxis): 
+    ''' Returns a textfile with all data points of psi for every time and space imcrement
+    
+    Parameters
+    - - - - - - -
+    array : multidimensional array
+    
+    filename: str
+        name of textfile
+    
+    spacedwith: str
+        Seperates all data points in the outputed txt file.
+    
+    txtaxis: Boolean
+        Displays txt file axis to show time and space outputs.'''
+#a function that takes the inputted array and outputs it, in the form of a text file,
+# with an optional axis.
+    with open(filename, 'w') as file: #opens the file. 
+        if txtaxis ==True:        
+            file.writelines(" Psi in Time-->") 
             file.writelines("\n")
             file.writelines("Psi in Space")
             file.writelines("\n")
